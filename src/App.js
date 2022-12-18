3481e7a7a6e091011ec40b2868c23cd0f0d325c8
kevinrss01 <kevin.rousseau3@gmail.com>
2022-12-18 08:50:23 +0100

    progress, navbar created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import './style/sass/main.scss'
+import {HomePage} from './pages/HomePage';
+import {Navbar} from './pages/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="app">
+            <div className="navbarContainer">
+                <Link to="/"><h1>Accueil</h1></Link>
+            </div>
+            <div className="routes">
+                <Routes>
+                    <Route path="/" element={HomePage}></Route>
+                </Routes>
+            </div>
+        </div>
+    </Router>
   );
 }
 
